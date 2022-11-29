639063418da230feef8d9237278b146a49a0f8a8
cristian250 <cristianvip39@gmail.com>
2022-11-28 18:16:57 -0600

    add progra

---
+++ parcial2/tarea6/MatrizIdentidad.java
@@ -0,0 +1,26 @@
+package parcial2.tarea6;
+
+public class MatrizIdentidad {
+    public static void main(String[]args){
+
+        //Matriz Ingresada.
+        int a [][]={
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+        };
+        System.out.println("Matriz Identidad: ");
+        
+        for (int i = 0; i < a.length; ++i){
+            a[i][i] = 1;
+        }
+
+        for (int i = 0; i < 4; ++i){
+            for (int j = 0; j < 4; ++j){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
